7442aac9bb759d962d42d62267097f068e0e2281
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2022-09-08 02:01:42 +0900

    Improving result page

--- app/search.py
+++ app/search.py
@@ -1,18 +1,19 @@
 import json
-from typing import List
+from typing import Dict, List
 
 with open("app/static/okinawa_01.json", 'r') as raw_file:
     oki_dict = json.load(raw_file)
-key_dict = {}
+key_dict:Dict[str, List[int]] = {}
 content_dict = {}
 for i, entry in enumerate(oki_dict):
     for index in entry["index"]:
-        key_dict[index] = i
+        key_list = key_dict.setdefault(index, [])
+        key_dict[index] = key_list + [i]
     content_dict[i] = entry
 
 
 def search(word: str) -> List:
-    key = key_dict.get(word)
-    if not key:
+    keys = key_dict.get(word)
+    if not keys:
         return []
-    return [content_dict[key]]
+    return [content_dict[key] for key in keys]
