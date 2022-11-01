5815a84799ebae931947d320249d5ef0cc547921
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2022-11-01 17:45:41 +0900

    search.py refactored

--- app/search.py
+++ app/search.py
@@ -1,40 +1,6 @@
-import json
 from typing import Callable, Dict, List, Tuple
 
-from wanakana import to_hiragana, to_katakana
-
-with open("app/static/okinawa_01.json", 'r') as raw_file:
-    raw_oki_dict = json.load(raw_file)
-
-with open("app/static/okinawa_02.json", 'r') as raw_file:
-    raw_yamato_dict = json.load(raw_file)
-
-
-class Dictionary():
-    def __init__(self, word_dict):
-        key_dict: Dict[str, List[int]] = {}
-        content_dict = {}
-        for i, entry in enumerate(word_dict):
-            for index in entry["index"]:
-                key_list = key_dict.setdefault(index, [])
-                key_dict[index] = key_list + [i]
-            content_dict[i] = entry
-        self._key_dict = key_dict
-        self._content_dict = content_dict
-
-    @property
-    def index_words(self):
-        return self._key_dict.keys()
-
-    def get_keys(self, index_word: str) -> List[int]:
-        return self._key_dict[index_word]
-
-    def get_content(self, key: int):
-        return self._content_dict[key]
-
-
-oki_dict = Dictionary(raw_oki_dict)
-yamato_dict = Dictionary(raw_yamato_dict)
+from app.dictionary import Dictionary, oki_dict, yamato_dict
 
 
 def _get_dict(dict_type: str) -> Dictionary:
@@ -65,11 +31,7 @@ def search(word: str,
            dict_type: str) -> List[List[str]]:
     results_dict: Dict[Tuple[int, ...], List[str]] = {}
     dictionary = _get_dict(dict_type)
-    if dict_type == "oki2yamato":
-        word = to_katakana(word)
-    elif dict_type == "yamato2oki":
-        word = to_hiragana(word)
-    index_filter = _get_filter(word, search_type)
+    index_filter = _get_filter(dictionary.normalise_kana(word), search_type)
     for index_word in dictionary.index_words:
         if index_filter(index_word):
             id_keys = tuple(dictionary.get_keys(index_word))
