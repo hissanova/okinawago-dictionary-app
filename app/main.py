1025acb01f0f132b055af7cdb2b111b5ed1d0f52
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2022-11-28 16:25:50 +0900

    first commit

--- app/main.py
+++ app/main.py
@@ -62,3 +62,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
